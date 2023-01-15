1a1bc834ba884215076c0e241495b843fd91143a
Stereo2025 <teneloks@gmail.com>
2023-01-15 14:17:45 +0300

    test2

--- tests/ads/ads_detail_test.py
+++ tests/ads/ads_detail_test.py
@@ -1,21 +1,21 @@
-# import pytest
-#
-#
-# @pytest.mark.django_db
-# def test_ads_create(client, ad, user_token):
-#     expected = {
-#         'id': 1,
-#         'name': '',
-#         'category': '',
-#         'author': 1,
-#         'price': 10,
-#         'description': None,
-#         'is_published': False,
-#         'image': None
-#     }
-#     response = client.get(
-#         f"/ad/{ad.id}/",
-#         HTTP_AUTHORIZATION="Bearer " + user_token)
-#
-#     assert response.status_code == 200
-#     assert response.data == expected
+import pytest
+
+
+@pytest.mark.django_db
+def test_ads_create(client, ad, user_token):
+    expected = {
+        'id': 2,
+        'name': '',
+        'category': '',
+        'author': 3,
+        'price': 10,
+        'description': None,
+        'is_published': False,
+        'image': None
+    }
+    response = client.get(
+        f"/ad/{ad.id}/",
+        HTTP_AUTHORIZATION="Bearer " + user_token)
+
+    assert response.status_code == 200
+    assert response.data == expected
